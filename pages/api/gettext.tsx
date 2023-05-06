5eac0b9fe5cb30e9c85329f410f49cfc455b2ac5
@bashybaranaba <bashybaranaba@gmail.com>
2023-05-06 17:18:27 +0300

    first commit

---
+++ pages/api/gettext.tsx
@@ -0,0 +1,27 @@
+import { getDocument } from "pdfjs-dist";
+
+export default async function handler(req: any, res: any) {
+  const { method } = req;
+
+  if (method === "POST") {
+    try {
+      const fileUrl = req.body.fileURL;
+      const loadingTask = getDocument(`${fileUrl}`);
+      const pdf = await loadingTask.promise;
+      const maxPages = pdf.numPages;
+      let rawText = "";
+      for (let i = 1; i <= maxPages; i++) {
+        const page = await pdf.getPage(i);
+        const textContent = await page.getTextContent();
+        const pageText = textContent.items
+          .map((item: any) => item.str)
+          .join("");
+        rawText += pageText + "\n";
+      }
+
+      res.status(200).json({ success: true, text: rawText });
+    } catch (err) {
+      res.status(500).json("error", err);
+    }
+  }
+}
